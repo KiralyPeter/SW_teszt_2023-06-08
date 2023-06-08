73d8fb20e09cb2c5e985d3f76840bf5956312e74
Peter Kiraly <peter.kiral@gmail.com>
2023-06-08 19:10:54 +0200

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/Button';
+
+// import { Button } from 'react-native-web';
 
 export default function App() {
+
+  const [count, setCounter] = useState(0);
+
+  function increment(){
+    // alert(count+1)
+    let sum = count+1;
+    setCounter(sum);    
+  }
+
+  function decrement(){
+    // alert(count+1)
+    let sum = count-1;
+    setCounter(sum);    
+  }
+
+  function reset(){
+    // alert(count+1)    
+    setCounter(0);    
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Forgalom</Text>
+      <Text style={styles.title}>Számláló</Text>
+      <Text style = {styles.count}>{count}</Text>
+
+      <Button title="Increase"
+      onPress={increment}/>
+
+      <Button title="Decrease"
+      onPress={decrement}/>
+
+      <Button title="Reset"
+      onPress={reset}/>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    color: 'white',
+  },
+  count: {
+    fontSize: 54,
+    color: 'yellow',
   },
 });
